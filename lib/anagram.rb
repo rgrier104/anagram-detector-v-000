fe7205c4cdc014dd0dc75aaf68815d399d9f9040
Rachel Grier <rgrier104@gmail.com>
2018-09-19 03:16:06 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,6 +5,18 @@ class Anagram
 
   def initialize(word)
     @word = word
-  
+  end
+
+  def match(array)
+    matches = []
+    word_letters = @word.split("")
+    array.each do |word|
+      if word.split("").sort == word_letters.sort
+        matches << word
+      end
+    end
+  matches
+end
+
 
 end
